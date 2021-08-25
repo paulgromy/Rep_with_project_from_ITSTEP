3013954878704c3bd85335d5dc4c0175576b6b36
Pavel Hramyko <kalipso_g@tut.by>
2021-08-25 21:50:34 +0300

    second commit

--- news/views.py
+++ news/views.py
@@ -8,19 +8,13 @@ from news.models import News, Category
 # И здесь же, прописываю путь к шаблону.
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.order_by()
     return render(request, "news/index.html", {"news": news,
-                                               "title": "Список новостей",
-                                               "categories": categories,})
-
-
+                                               "title": "Список новостей"})
 
 def give_categories(request, id_category):
     news = News.objects.filter(category_id = id_category)
-    categories = Category.objects.all()
     category = Category.objects.get(pk = id_category)
     return render(request, "news/category.html", {"news": news,
-                                                  "categories": categories,
                                                   "category": category})
 
 
