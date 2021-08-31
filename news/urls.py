80b1b8d7ed61e4c3e3070a0152a35d53e4b0e899
Pavel Hramyko <kalipso_g@tut.by>
2021-08-31 21:39:36 +0300

    Add class based view and business-logic

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", index, name = "Main"),
-    path("category/<int:id_category>/", give_categories, name = "Category"),
+    path("", HomeNews.as_view(), name="Main"),
+    path("category/<int:id_category>/", GiveCategories.as_view(), name="Category"),
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 from news.models import News, Category
+from django.views.generic import ListView
 
 
-# Создаю представление, которое будет генерировать, имеющиеся в наличии новости.
-# И здесь же, прописываю путь к шаблону.
-def index(request):
-    news = News.objects.all()
-    return render(request, "news/index.html", {"news": news,
-                                               "title": "Список новостей"})
+# Создаю представлениe в виде класса, которое будет генерировать, имеющиеся в наличии новости.
+# От самой свежей, до последней. Вне зависимости от её категории.
+class HomeNews(ListView):
+    model = News
+    template_name = "news/home_news_list.html"
+    context_object_name = "news"
+    allow_empty = False
 
-def give_categories(request, id_category):
-    news = News.objects.filter(category_id = id_category)
-    category = Category.objects.get(pk = id_category)
-    return render(request, "news/category.html", {"news": news,
-                                                  "category": category})
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomeNews, self).get_context_data(**kwargs)
+        context["title"] = "Главная страница"
+        return context
+
+# То же самое в виде функции...
+# def index(request):
+#     news = News.objects.all()
+#     return render(request, "news/index.html", {"news": news,
+#                                                "title": "Список новостей"})
 
 
+# Вьюха в виде класса для вывода новостей по категориям.
+class GiveCategories(ListView):
+    model = News
+    template_name = "news/home_news_list.html"
+    context_object_name = "news"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(GiveCategories, self).get_context_data(**kwargs)
+        context["title"] = Category.objects.get(pk=self.kwargs["id_category"])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs["id_category"])
+
+# Тоже самое ввиде функции
+# def give_categories(request, id_category):
+#     news = News.objects.filter(category_id=id_category)
+#     category = Category.objects.get(pk=id_category)
+#     return render(request, "news/category.html", {"news": news,
+#                                                   "category": category})
